2cc9edbfdb6f7747bb73cbc3be4ada8a866abd60
adatlaborhu <tomi@adatlabor.hu>
2016-04-23 03:02:20 +0200

    jobb mint a regi

--- howdy.py
+++ howdy.py
@@ -7,24 +7,27 @@ def conversation(question):
     knowledge = []
     with open('knowledge.csv', 'r') as content_file:
 	    for line in content_file:
-    		knowledge.append(line.strip().split(','))
+    		knowledge.append(line.strip().split(';;;'))
+	    content_file.close()
 
 #Check if there is answer in the lists. If yes, gives back the answer. If no, gives back the question.
     answer = ""
     for line in knowledge:
 	if line[0] == question:
-	    print line[1]
+	    print "Howdy: " + line[1]
 	    answer = line[1]
+	    next_question = raw_input()
+	    target = open('knowledge.csv', 'a')
+	    target.write(answer + ";;;" + next_question + ";;;" + "1" + "\n")
+	    target.close()
+	    return next_question
     if answer == "":
-	    print question
-
-    next_question = raw_input()
-    return next_question
-
-#    target = open('knowledge.csv', 'a')
-#    target.write(question)
-#    target.write("\n")
-#    target.close()
+	    print "Howdy: " + question
+	    next_question = raw_input("You: ")
+	    target = open('knowledge.csv', 'a')
+	    target.write(question + ";;;" + next_question + ";;;" + "1" + "\n")
+	    target.close()
+	    return next_question
 
 
 #==========================
@@ -34,7 +37,7 @@ print "Hi, my name is Howdy! I don't know anything about the world. Please teach
 
 #Starts with user input.
 question = ""
-question = raw_input()
+question = raw_input("You: ")
 
 #calling the conversation function
 while question != "exit":
